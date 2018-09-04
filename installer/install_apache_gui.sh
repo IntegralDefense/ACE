cebd488557511a1bb4b38fa50985bb02cd89d1b2
John Davison <unixfreak0037@gmail.com>
2018-09-04 08:23:13 -0400

    moved saq_apache.conf to saq_apache.example.conf and modified installer to install it

--- installer/install_apache_gui.sh
+++ installer/install_apache_gui.sh
@@ -25,6 +25,7 @@ rm ~/.mod_wsgi-express.output
 sudo a2enmod wsgi_express
 sudo a2enmod ssl
 sudo a2ensite default-ssl
+cp /opt/ace/etc/saq_apache.example.conf /opt/ace/etc/saq_apache.conf
 sudo ln -s /opt/ace/etc/saq_apache.conf /etc/apache2/sites-available/ace.conf && \
 sudo a2ensite ace && \
 sudo systemctl restart apache2.service
