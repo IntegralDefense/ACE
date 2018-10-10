94b26f09bb7217a583dce6cf92393c75e254d97d
John Davison <unixfreak0037@gmail.com>
2018-10-10 01:23:54 +0000

    closes #22

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash, session
-from flask.ext.login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from .forms import LoginForm
 from ..models import User
