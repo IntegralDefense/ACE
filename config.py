d56372af4a0540e7ad6ffcb3748e293cf50740d8
John Davison <unixfreak0037@gmail.com>
2018-12-19 01:18:52 +0000

    modified sqlalchemy usage to support ssl

--- config.py
+++ config.py
@@ -26,6 +26,22 @@ class Config(object):
         'pool_size': 5,
     }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # are we using SSL for MySQL connections? (you should be)
+        if 'ssl_ca' in saq.CONFIG['database_ace'] \
+        or 'ssl_cert' in saq.CONFIG['database_ace'] \
+        or 'ssl_key' in saq.CONFIG['database_ace']:
+            ssl_options = { 'ca': saq.CONFIG['database_ace']['ssl_ca'] }
+            if 'ssl_cert' in saq.CONFIG['database_ace']:
+                ssl_options['cert'] = saq.CONFIG['database_ace']['ssl_cert']
+            if 'ssl_key' in saq.CONFIG['database_ace']:
+                ssl_options['key'] = saq.CONFIG['database_ace']['ssl_key']
+
+            self.SQLALCHEMY_DATABASE_OPTIONS['connect_args'] = {}
+            self.SQLALCHEMY_DATABASE_OPTIONS['connect_args']['ssl'] = ssl_options
+
     @staticmethod
     def init_app(app):
         pass
@@ -42,7 +58,7 @@ class DevelopmentConfig(Config):
 
 # the keys for this dict match the instance_type config setting in global section of etc/saq.ini
 config = {
-    'DEV': DevelopmentConfig,
-    'QA': ProductionConfig,
-    'PRODUCTION': ProductionConfig,
+    'DEV': DevelopmentConfig(),
+    'QA': ProductionConfig(),
+    'PRODUCTION': ProductionConfig(),
 }
