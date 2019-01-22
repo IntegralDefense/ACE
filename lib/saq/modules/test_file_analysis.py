5ca36ce09716ca068de45bc8d79b5d709f3b3f18
Cole Robinette <robinette.31@gmail.com>
2019-01-22 16:44:05 -0500

    fixes #126 - decompile jar files containing less than max_jar_file_count files, added unit tests for jar decompiling, fixed test data dir creation bug, added download of decompiler binary to installation script

--- lib/saq/modules/test_file_analysis.py
+++ lib/saq/modules/test_file_analysis.py
@@ -358,6 +358,54 @@ class FileAnalysisModuleTestCase(ACEModuleTestCase):
         self.assertEquals(analysis.file_count, 1)
         _file = analysis.get_observables_by_type(F_FILE)
         self.assertEquals(len(_file), 1)
+
+    def test_file_analysis_002_archive_003_jar(self):
+
+        root = create_root_analysis(uuid=str(uuid.uuid4()))
+        root.initialize_storage()
+        shutil.copy('test_data/jar/test.jar', root.storage_dir)
+        _file = root.add_observable(F_FILE, 'test.jar')
+        root.save()
+        root.schedule()
+
+        engine = TestEngine()
+        engine.enable_module('analysis_module_archive', 'test_groups')
+        engine.enable_module('analysis_module_file_type', 'test_groups')
+        engine.controlled_stop()
+        engine.start()
+        engine.wait()
+
+        root.load()
+        _file = root.get_observable(_file.id)
+        
+        from saq.modules.file_analysis import ArchiveAnalysis
+        analysis = _file.get_analysis(ArchiveAnalysis)
+        self.assertIsNotNone(analysis)
+        self.assertEquals(analysis.file_count, 42)
+
+    def test_file_analysis_002_archive_004_jar(self):
+
+        root = create_root_analysis(uuid=str(uuid.uuid4()))
+        root.initialize_storage()
+        shutil.copy('test_data/jar/too_many_files.jar', root.storage_dir)
+        _file = root.add_observable(F_FILE, 'too_many_files.jar')
+        root.save()
+        root.schedule()
+
+        engine = TestEngine()
+        engine.enable_module('analysis_module_archive', 'test_groups')
+        engine.enable_module('analysis_module_file_type', 'test_groups')
+        engine.controlled_stop()
+        engine.start()
+        engine.wait()
+
+        root.load()
+        _file = root.get_observable(_file.id)
+        
+        from saq.modules.file_analysis import ArchiveAnalysis
+        analysis = _file.get_analysis(ArchiveAnalysis)
+        self.assertIsNotNone(analysis)
+        self.assertFalse(analysis)
         
     def test_file_analysis_003_xml_000_rels(self):
 
