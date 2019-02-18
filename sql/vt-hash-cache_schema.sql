327b03b303389f5ac7ac982280ff56139aaa7111
John Davison <unixfreak0037@gmail.com>
2019-02-18 15:13:40 +0000

    updated sql schemas

--- sql/vt-hash-cache_schema.sql
+++ sql/vt-hash-cache_schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
 --
 -- Host: localhost    Database: vt-hash-cache
 -- ------------------------------------------------------
--- Server version	5.5.54-0ubuntu0.14.04.1
+-- Server version	5.7.25-0ubuntu0.18.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,7 +34,7 @@ CREATE TABLE `result_cache` (
   KEY `i_sha1` (`sha1`),
   KEY `i_sha2` (`sha2`),
   KEY `i_insert_date` (`insert_date`)
-) ENGINE=InnoDB AUTO_INCREMENT=4708752 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +46,4 @@ CREATE TABLE `result_cache` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-22  9:00:26
+-- Dump completed on 2019-02-18 15:08:23
