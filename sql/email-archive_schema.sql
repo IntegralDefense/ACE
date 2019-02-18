a1ba712139ec22df0d55e60e9f66017f70d02699
John Davison <unixfreak0037@gmail.com>
2019-02-18 15:13:40 +0000

    updated sql schemas

--- sql/email-archive_schema.sql
+++ sql/email-archive_schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
 --
--- Host: localhost    Database: email-archive-vvv-v3
+-- Host: localhost    Database: email-archive
 -- ------------------------------------------------------
--- Server version	5.5.53-0ubuntu0.14.04.1
+-- Server version	5.7.25-0ubuntu0.18.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,7 +31,7 @@ CREATE TABLE `archive` (
   UNIQUE KEY `server_id` (`server_id`,`md5`),
   KEY `idx_insert_date` (`insert_date`),
   CONSTRAINT `fk_archive_1` FOREIGN KEY (`server_id`) REFERENCES `archive_server` (`server_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2176919 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ DROP TABLE IF EXISTS `archive_index`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `archive_index` (
-  `field` enum('env_from','env_to','body_from','body_to','subject','decoded_subject','message_id','url','content') NOT NULL,
+  `field` enum('env_from','env_to','body_from','body_to','subject','decoded_subject','message_id','content','url') NOT NULL,
   `hash` binary(16) NOT NULL,
   `archive_id` int(11) NOT NULL,
   PRIMARY KEY (`hash`,`archive_id`,`field`),
@@ -60,15 +60,15 @@ DROP TABLE IF EXISTS `archive_search`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `archive_search` (
-  `field` enum('env_from','env_to','body_from','body_to','subject','decoded_subject','message_id','url','content') NOT NULL,
-  `value` varbinary(2083) NOT NULL,
+  `field` enum('env_from','env_to','body_from','body_to','subject','decoded_subject','message_id','content','url') NOT NULL,
+  `value` varbinary(512) NOT NULL,
   `archive_id` int(11) NOT NULL,
-  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`entry_id`),
-  UNIQUE KEY `entry_id_UNIQUE` (`entry_id`),
+  PRIMARY KEY (`field`,`value`,`archive_id`),
+  KEY `value` (`value`),
+  KEY `field_value` (`field`,`value`),
   KEY `archive_id` (`archive_id`),
-  KEY `field_value` (`field`,`value`(767)) KEY_BLOCK_SIZE=256
-) ENGINE=InnoDB AUTO_INCREMENT=5014 DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_archive_search_1` FOREIGN KEY (`archive_id`) REFERENCES `archive` (`archive_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +83,7 @@ CREATE TABLE `archive_server` (
   `hostname` varchar(256) NOT NULL,
   PRIMARY KEY (`server_id`),
   UNIQUE KEY `hostname` (`hostname`)
-) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -95,4 +95,4 @@ CREATE TABLE `archive_server` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-14 11:13:56
+-- Dump completed on 2019-02-18 15:08:23
