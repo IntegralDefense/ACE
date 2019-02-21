166322d754ebb0ae71ac57302bef5cb7c5641052
Cole Robinette <robinette.31@gmail.com>
2019-02-21 15:12:36 -0500

    added remediation_target observable type

--- lib/saq/observables/__init__.py
+++ lib/saq/observables/__init__.py
@@ -40,6 +40,7 @@ __all__ = [
     'YaraRuleObservable',
     'IndicatorObservable',
     'MD5Observable',
+    'RemediationTarget',
     'SHA1Observable',
     'SHA256Observable',
     'EmailConversationObservable',
@@ -506,6 +507,14 @@ class MD5Observable(CaselessObservable):
     def jinja_available_actions(self):
         return [ ObservableActionUploadToCrits() ]
 
+class RemediationTarget(CaselessObservable):
+    def __init__(self, *args, **kwargs):
+        super().__init__(F_REMEDIATION_TARGET, *args, **kwargs)
+
+    @property
+    def jinja_available_actions(self):
+        return [ ]
+
 class SHA1Observable(CaselessObservable):
     def __init__(self, *args, **kwargs):
         super().__init__(F_SHA1, *args, **kwargs)
@@ -597,6 +606,7 @@ _OBSERVABLE_TYPE_MAPPING = {
     F_INDICATOR: IndicatorObservable,
     F_IPV4: IPv4Observable,
     F_MD5: MD5Observable,
+    F_REMEDIATION_TARGET: RemediationTarget,
     F_SHA1: SHA1Observable,
     F_SHA256: SHA256Observable,
     F_SUSPECT_FILE: FileObservable,
