bb75bd576677a41ffbed1f89102de3523dc75893
Cole Robinette <robinette.31@gmail.com>
2019-04-26 10:29:19 -0400

    added observable_tag_mapping table to schema, added fetch_tags function to lib.saq.analysis.Observable and lib.saq.observables.FileObservable classes, call fetch_tags when observable is added to analysis

--- sql/ace_schema.v5.5.sql
+++ sql/ace_schema.v5.5.sql
@@ -476,6 +476,23 @@ CREATE TABLE `tags` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `observable_tag_mapping`
+--
+
+DROP TABLE IF EXISTS `observable_tag_mapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_mapping` (
+  `tag_id` int(11) NOT NULL,
+  `observable_id` int(11) NOT NULL,
+  PRIMARY KEY (`tag_id`,`observable_id`),
+  KEY `observable_tag_mapping_ibfk_2` (`observable_id`),
+  CONSTRAINT `fk_observable_tag_mapping_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_observable_tag_mapping_2` FOREIGN KEY (`observable_id`) REFERENCES `observables` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `users`
 --
 
