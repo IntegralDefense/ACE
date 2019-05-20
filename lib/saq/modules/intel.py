18a7f2f0a7219d58ddf1309b1062f04acbbef762
John Davison <unixfreak0037@gmail.com>
2019-05-20 17:11:29 +0000

    dedup sources for new sip intel modules

--- lib/saq/modules/intel.py
+++ lib/saq/modules/intel.py
@@ -45,7 +45,7 @@ class IntelAnalysis(Analysis):
                 campaigns = '(no campaign)'
 
         if 'references' in self.details:
-            sources = ','.join([x['source'] for x in self.details['references']])
+            sources = list(set(','.join([x['source'] for x in self.details['references']])))
             if not sources:
                 sources = '(no sources)'
        
